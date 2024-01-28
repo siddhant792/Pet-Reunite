6cc001511ad58da13759b35a93358b3a0cbb5136
prashantinim <prashantinimaniam11@gmail.com>
2024-01-28 17:42:27 +1100

    found pet

--- PR-Member-Desktop/found_pet.js
+++ PR-Member-Desktop/found_pet.js
@@ -31,4 +31,20 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => console.log('Success:', data))
         .catch((error) => console.error('Error:', error));
     });
+
+    document.getElementById('shelterLink').onclick = function(event) {
+        event.preventDefault();
+        document.getElementById('myModal').style.display = 'block';
+    }
+    
+    document.getElementsByClassName('close')[0].onclick = function() {
+        document.getElementById('myModal').style.display = 'none';
+    }
+    
+    window.onclick = function(event) {
+        if (event.target == document.getElementById('myModal')) {
+            document.getElementById('myModal').style.display = 'none';
+        }
+    }
+    
 });
