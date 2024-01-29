e3914804ed12c5c4e9c26e339ea8c7a36cb2070f
prashantinim <prashantinimaniam11@gmail.com>
2024-01-30 03:58:04 +1100

    formatting pop up

--- PR-Member-Desktop/Found Pet report/found_pet.js
+++ PR-Member-Desktop/Found Pet report/found_pet.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         shelters.forEach(shelter => {
             const shelterDiv = document.createElement('div');
-            shelterDiv.className = 'div-4';
+            shelterDiv.className = 'div-30';
             shelterDiv.innerHTML = `
             <div class="div-30">
             <div class="div-31">Animal Shelters</div>
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 <img
                   loading="lazy"
                   srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/22375695048c4830128a6ff496d5af30cb21cfad52d244791573865ba6394e9e?apiKey=6c234474d5484635a95c9b3a2387c63f&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/22375695048c4830128a6ff496d5af30cb21cfad52d244791573865ba6394e9e?apiKey=6c234474d5484635a95c9b3a2387c63f&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/22375695048c4830128a6ff496d5af30cb21cfad52d244791573865ba6394e9e?apiKey=6c234474d5484635a95c9b3a2387c63f&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/22375695048c4830128a6ff496d5af30cb21cfad52d244791573865ba6394e9e?apiKey=6c234474d5484635a95c9b3a2387c63f&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/22375695048c4830128a6ff496d5af30cb21cfad52d244791573865ba6394e9e?apiKey=6c234474d5484635a95c9b3a2387c63f&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/22375695048c4830128a6ff496d5af30cb21cfad52d244791573865ba6394e9e?apiKey=6c234474d5484635a95c9b3a2387c63f&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/22375695048c4830128a6ff496d5af30cb21cfad52d244791573865ba6394e9e?apiKey=6c234474d5484635a95c9b3a2387c63f&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/22375695048c4830128a6ff496d5af30cb21cfad52d244791573865ba6394e9e?apiKey=6c234474d5484635a95c9b3a2387c63f&"
-                  class="img"
+                  class="img-a1"
                 />
               </div>
               <div class="div-36">
@@ -44,32 +44,32 @@ document.addEventListener('DOMContentLoaded', function() {
                   <img
                         loading="lazy"
                         srcset="https://cdn.builder.io/api/v1/image/assets/TEMP/24fd3464581da9067f60b4d924d02485d5af8a355aad509987ba502f9d051bef?apiKey=6c234474d5484635a95c9b3a2387c63f&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/24fd3464581da9067f60b4d924d02485d5af8a355aad509987ba502f9d051bef?apiKey=6c234474d5484635a95c9b3a2387c63f&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/24fd3464581da9067f60b4d924d02485d5af8a355aad509987ba502f9d051bef?apiKey=6c234474d5484635a95c9b3a2387c63f&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/24fd3464581da9067f60b4d924d02485d5af8a355aad509987ba502f9d051bef?apiKey=6c234474d5484635a95c9b3a2387c63f&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/24fd3464581da9067f60b4d924d02485d5af8a355aad509987ba502f9d051bef?apiKey=6c234474d5484635a95c9b3a2387c63f&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/24fd3464581da9067f60b4d924d02485d5af8a355aad509987ba502f9d051bef?apiKey=6c234474d5484635a95c9b3a2387c63f&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/24fd3464581da9067f60b4d924d02485d5af8a355aad509987ba502f9d051bef?apiKey=6c234474d5484635a95c9b3a2387c63f&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/24fd3464581da9067f60b4d924d02485d5af8a355aad509987ba502f9d051bef?apiKey=6c234474d5484635a95c9b3a2387c63f&"
-                        class="img-2"
+                        class="img-a2"
                   />
-                    <div class="div-40">Any Pets</div>
-                  </div>
-                  <div class="div-41">
-                    <img
-                      loading="lazy"
-                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/06ab140737b12e567583c2a6a480e0a85e0a66194026ae167dcfda1d494e1627?apiKey=6c234474d5484635a95c9b3a2387c63f&"
-                      class="img-3"
-                    />
+                
                     <div class="div-42">
-                      (
+                      
                       <span style="font-size: 13px">${shelter.phone}</span>
                     </div>
                   </div>
-                  <div class="div-43">
-                    <img
-                      loading="lazy"
-                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/a89d298cf024d66ffdc2917a3a7ba6c937f077763ab7b0990b5febe07793d5f9?apiKey=6c234474d5484635a95c9b3a2387c63f&"
-                      class="img-4"
-                    />
-                    <div class="div-44">${shelter.openingHours}</div>
+                  <div class="div-39">
+                        <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/a89d298cf024d66ffdc2917a3a7ba6c937f077763ab7b0990b5febe07793d5f9?apiKey=6c234474d5484635a95c9b3a2387c63f&"
+                        class="img-a4"
+                        />
+        
+                    <div class="div-42">
+                      
+                      <span style="font-size: 13px">${shelter.openingHours}</span>
+                    </div>
+                   
                   </div>
+                 
+                  </div>
+                  <button class="div-45">Select</button>
                 </div>
               </div>
-              <button class="div-45">Select</button>
             </div>
           </div>
             `;
